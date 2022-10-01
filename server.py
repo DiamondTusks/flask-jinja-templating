0ddaff58e2f3143373d80e83786a759df7a27e8b
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-10-01 18:48:03 +1000

    Add files via upload

--- server.py
+++ server.py
@@ -24,6 +24,12 @@ def guess(name):
 
     return render_template("guess.html", name=name, gender=guessed_gender, age=guessed_age)
 
+@app.route('/blog/<num>')
+def get_blog(num):
+    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
